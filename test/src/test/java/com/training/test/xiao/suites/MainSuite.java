507c4e8da3ed53c2d8a901e9d43c16653dd47431
li.xiao <li.xiao@CDL5MMRYZ1.symbio.com>
2018-07-05 12:20:10 +0800

    updates

---
+++ test/src/test/java/com/training/test/xiao/suites/MainSuite.java
@@ -0,0 +1,12 @@
+package com.training.test.xiao.suites;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import com.training.test.xiao.cases.*;
+ 
+@RunWith(Suite.class)
+@Suite.SuiteClasses({ Suite1.class, Suite2.class })
+ 
+public class MainSuite {
+ 
+}
