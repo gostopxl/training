e504e318e25f79ba1b6a8600d934c019f584dcc3
yongcheng.diao <yongcheng.diao@CDTEMP16.symbio.com>
2018-07-06 09:52:45 +0800

    Automated testing of Amazon sites

---
+++ test/src/test/java/com/training/test/diao/basic/Browsers.java
@@ -0,0 +1,17 @@
+package com.training.test.diao.basic;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Browsers {
+	private static WebDriver webDriver;
+	public static WebDriver wDriver() {
+		System.setProperty("webdriver.gecko.driver","lib/geckodriver.exe");
+		webDriver = new FirefoxDriver();
+		webDriver.manage().window().maximize();
+		webDriver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		return webDriver;
+	}
+}
---
+++ test/src/test/java/com/training/test/diao/cases/AmazonTest.java
@@ -0,0 +1,50 @@
+package com.training.test.diao.cases;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.*;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.interactions.Actions;
+
+import com.training.test.diao.basic.Browsers;
+import com.training.test.diao.pages.AmazonPages;
+
+/**
+ * Unit test for simple App.
+ */
+public class AmazonTest{
+	private WebDriver driver;
+	@Before
+	public void setUp() throws Exception {
+		driver = Browsers.wDriver();
+	}
+
+	@Test
+	public void testUntitledTestCase() throws Exception {
+		//open the web page
+		driver.get("https://www.amazon.com/");
+		//Initialize the page object
+		AmazonPages AP = new AmazonPages(driver);
+		//Initialize the actions object
+		Actions action = new Actions(driver);	
+		//Mouse mobile hovering
+		action.moveToElement(AP.departmentsSpan).perform();
+		Thread.sleep(2000);
+		AP.AppstoreSpan.click();
+		action.moveToElement(AP.AppstoreSpan).perform();		
+		Thread.sleep(2000);
+		AP.GameSpan.click();		
+	
+		assertEquals("Minecraft", AP.GameName.getText());		
+		AP.GameName.click();
+		AP.AddToList.click();
+		AP.ContinueBtn.click();
+		assertEquals("Enter your email or mobile phone number", AP.ErrMsg.getText().trim());
+	}
+		
+	@After
+	public void tearDown() throws Exception {
+		driver.quit();
+	
+	}
+
+}
